44d332223f5e78313512da3b425608ca4d6c74d9
Asilbek <anvarsaparov374@gmail.com>
2024-03-27 15:16:54 +0500

    done

---
+++ lesson-50/index.js
@@ -0,0 +1,32 @@
+//  2 types of function
+// Function declaration
+
+function calcFunc(a){
+    let result = a*a;
+    return result;
+    console.log("This code doesn't work");
+}
+console.log(calcFunc(9));
+
+// Function expression
+
+const MyFunc = function(){
+    console.log("Hello world!");
+}
+MyFunc()
+
+function ayirish(a,b){
+    return a-b
+}
+
+const hisoblash = function(a,b){
+    return a-b
+}
+console.log(ayirish(3,2));
+console.log(hisoblash(10,2));
+
+// Arrow function
+const kvadrat = (son) => {
+    return son*son
+}
+console.log(kvadrat(7));
